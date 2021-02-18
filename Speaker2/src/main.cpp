ad189edf60b790ada6a05e1ba440157fa22f269f
stefgielen <stef.gielen17@gmail.com>
2021-02-18 18:00:14 +0100

    code afgewerkt
    moet nog geverifieerd worden door test

---
+++ Speaker2/src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include "XT_DAC_Audio.h"
+#include "SoundData.h"
+
+XT_Wav_Class Sound(Force);
+XT_DAC_Audio_Class DacAudio(25,0);
+uint32_t DemoCounter=0;
+
+void setup() {
+  // put your setup code here, to run once:
+      Serial.begin(115200);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+DacAudio.FillBuffer();               
+  if(Sound.Playing==false) 
+    DacAudio.Play(&Sound);      
+  Serial.println(DemoCounter++);      
+}
